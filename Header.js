d85ae5634d5ab0789beefc43e548944cf9ea847c
Amélie Detrez <amelie.detrez@student.ehb.be>
2020-08-05 13:26:03 +0200

    mouse trail

--- Header.js
+++ Header.js
@@ -4,21 +4,21 @@ import * as Utils from "./context/utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
+let centerX = width / 2;
+let centerY = height / 2;
 
 document.onmousemove = move;
 
-function move(e) {
-    context.fillRect(0, 0, width, height);
-    console.log(e.pageX);
-    Utils.fillArc(e.pageX, e.pageY, 50, 0, 2 * Math.PI);
-    Utils.fillArc(e.pageX, e.pageY, 150, 0, 2 * Math.PI);
-    context.stroke();
-};
-
-(function header() {
+(function basicHeader() {
     context.fillRect(0, 0, width, height);
     context.textAlign = "center";
     context.font = "bold 80pt Bebas Neue";
     context.fillStyle = "white";
-    context.fillText("Amélie Detrez", width / 2, height / 2);
+    context.fillText("Amélie Detrez", centerX, centerY);
 })();
+
+function move(e) {
+    console.log(e.pageX);
+    context.fillStyle = "white";
+    Utils.fillEllipse(e.pageX, e.pageY, Utils.randomNumber(-25, 5), Utils.randomNumber(-25, 5));
+};
